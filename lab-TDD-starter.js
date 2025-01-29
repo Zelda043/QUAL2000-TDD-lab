312d14e682bd3f57d4c59665346877e0b845f85e
Paras <parasrawat0418@gmail.com>
2025-01-28 19:17:47 -0500

    Lab2 Submission

--- lab-TDD-starter.js
+++ lab-TDD-starter.js
@@ -13,23 +13,23 @@ node --test
 
 //add function
 const add = (a, b) => {
-  //ex: add(3,5) should return 5
+  //ex: add(3,5) should return 8
   //write your code here
-  return; //return the result
+  return a + b; //return the result
 };
 
 //subtract function
 const subtract = (a, b) => {
   //ex: subtract(5,9) should return -4
   //write your code here
-  return; //return the result
+  return a - b; //return the result
 };
 
 //multiply function
 const multiply = (a, b) => {
   //ex: multiply (12, 9) should return 108
   //write your code here
-  return; //return the result
+  return a * b; //return the result
 };
 
 //divide function
@@ -37,21 +37,25 @@ const divide = (a, b) => {
   //ex: divide(55, 5) should return 11
   //Make sure to test for zero division should throw and error
   //write your code here
-  return; //return the result
+  if (b === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  return a / b;
 };
 
 //modulas function
 const modulas = (a, b) => {
   //ex: modulas(17,4) should return 1
   //write your code here
-  return; //return the result
+  return a % b; //return the result
 };
 
 //power function
 const power = (a, b) => {
   //ex: power(2, 5) should return 32
   //write your code here
-  return; //return the result
+  const p = Math.pow(a, b);
+  return p; //return the result
 };
 
 //square function
@@ -59,21 +63,24 @@ const squareRoot = (a) => {
   //Make sure that squareRoot a negative number should throw an error
   //ex: squareRoot(25) should return 5
   //write your code here
-  return; //return the result
+  let c = Math.sqrt(a);
+  return c; //return the result
 };
 
 //floor function
 const floor = (a) => {
   //ex: floor(15.3) should return 15
   //write your code here
-  return; //return the result
+  let d = Math.floor(a);
+  return d; //return the result
 };
 
 //ceiling function
 const ceiling = (a) => {
   //ex: ceiling(15.3) should return 16
   //write your code here
-  return; //return the result
+  let e = Math.ceil(a);
+  return e; //return the result
 };
 
 ////////////////////////////////////////
